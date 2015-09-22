caf589e8ff6d3fd30045fbf154ab9ba6ba8b955d
chandramohan <chandramohan.kontu@gmail.com>
2015-09-22 21:49:37 +0530

    Shared folder added
    Shared folder added

--- AngularDemo/AngularDemo.APIServices/Controllers/EmployeeController.cs
+++ AngularDemo/AngularDemo.APIServices/Controllers/EmployeeController.cs
@@ -6,15 +6,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using AngularDemo.Shared;
 
 namespace AngularDemo.APIServices.Controllers
 {
-    public class EmployeeController : ApiController
+    public class EmployeeController : BaseController
     {
         EmployeeRepository _employeeRepository = new EmployeeRepository();
+        [MethodAspect]
         public List<Employee> GetEmployees()
-        {
-                        
+        {                       
             
             return _employeeRepository.GetEmployees();
         }
@@ -23,5 +27,9 @@ namespace AngularDemo.APIServices.Controllers
         {
             return _employeeRepository.getEmployee(id);
         }
+        //public override Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken)
+        //{
+        //    return base.ExecuteAsync(controllerContext, cancellationToken);
+        //}
     }
 }
